7c3d7b90d4d72e2ae95e405f6093fd181a1a51b5
HeyKathi <katharina.almer@sr.htlweiz.at>
2025-11-10 08:05:17 +0100

    Programm mit 2 Threads starten

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,22 +5,34 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-   
     
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
         
-        
+        Thread thread1 = new Thread();
+        Thread thread2 = new Thread();
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
     }
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 1; i == 100; i++)
+        {
+            Thread.Sleep(100);
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+        for (int i = 100; i == 1; i--)
+        {
+            Thread.Sleep(100);
+        }
     }
 }
