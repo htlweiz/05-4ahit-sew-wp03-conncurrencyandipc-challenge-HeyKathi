da0d736784986e0f71c7bb0f4ccaf447abba72d4
HeyKathi <katharina.almer@sr.htlweiz.at>
2025-11-10 08:10:26 +0100

    Verbesserte Version (Methoden bei den Threads angewandt)

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,13 +5,13 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-    
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
         
-        Thread thread1 = new Thread();
-        Thread thread2 = new Thread();
+        Thread thread1 = new Thread(CountUpThreadA);
+        Thread thread2 = new Thread(CountDownThreadB);
 
         thread1.Start();
         thread2.Start();
